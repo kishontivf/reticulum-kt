7844c5aee5e91ff7eb11f6e18af4043e399e5566
AdamSzucs-VKF <adam.szucs@kishonti.net>
2026-09-04 13:12:10 +0200

    Bump snapshot version

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 allprojects {
     group = "com.github.kishontivf.reticulum-kt"
-    version = System.getenv("VERSION")?.removePrefix("v") ?: "0.1.0-SNAPSHOT"
+    version = System.getenv("VERSION")?.removePrefix("v") ?: "0.2.0-SNAPSHOT"
 }
 
 dependencies {
